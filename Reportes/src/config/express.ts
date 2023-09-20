519934f663571de87f817f31d6948e856bd66079
AldairBH <84097102+AldairBH@users.noreply.github.com>
2023-09-19 18:59:10 -0600

    Add files via upload

---
+++ Reportes/src/config/express.ts
@@ -0,0 +1,21 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+
+const app = express();
+
+
+app.use(
+    cors({
+        origin: '*'
+    })
+);
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.json({limit: '20mb'}))
+
+app.get('/', (req, res) => {
+    res.send('Server running...');
+})
+
+export default
